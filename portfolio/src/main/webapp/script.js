1bbcc3816662d17e7c5a06865c849a58dc28c46d
Amber Michaca <aa.michaca7@gmail.com>
2021-06-04 15:16:44 +0000

    Add first draft of webpage

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['I love to cook!', 'I am on a hiphop team!', 'I am Mexican & Colombian', 'I love to travel'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
