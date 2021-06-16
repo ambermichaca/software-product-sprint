dc5f7dc887e9f2ee6773726dd04c470492070c00
Amber Michaca <aa.michaca7@gmail.com>
2021-06-16 17:02:14 +0000

    Add form to my webpage.

---
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -0,0 +1,29 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/form-handler")
+public class FormHandlerServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    // Get the value entered in the form.
+    String textValue = request.getParameter("text-input");
+
+    // Print the value so you can see it in the server logs.
+    System.out.println("You submitted: " + textValue);
+
+    // Write the value to the response so the user can see it.
+    response.getWriter().println("You submitted: " + textValue);
+
+    //redirects user to original URL once text is submitted 
+    response.sendRedirect("http://amichaca-sps-summer21.appspot.com/");
+
+    // i would like to print a statment that says thank you, I'll get back to you shortly
+  }
+}
