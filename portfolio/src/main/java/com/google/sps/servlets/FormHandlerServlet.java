c50d60105751383ab276644c47075a95edc69cc0
Amber Michaca <aa.michaca7@gmail.com>
2021-06-19 14:53:36 +0000

    Add datastore functionality

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -6,24 +6,46 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
+
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    // Get the value entered in the form.
-    String textValue = request.getParameter("text-input");
+    // Get the text entered in the form. Sanitize user input to remove HTML tags and JavaScript.
+    String email = Jsoup.clean(request.getParameter("email"), Whitelist.none());
+    long timestamp = System.currentTimeMillis();
 
-    // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + textValue);
+    //create instance of datastore 
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
 
-    // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + textValue);
+    //create key 
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("ContactInfo");
+
+    FullEntity contactInfoEntity =
+        Entity.newBuilder(keyFactory.newKey())
+            .set("email", email) 
+            .set("timestamp", timestamp)
+            .build();
+    datastore.put(contactInfoEntity); 
 
     //redirects user to original URL once text is submitted 
-    response.sendRedirect("http://amichaca-sps-summer21.appspot.com/");
+    response.sendRedirect("/index.html"); 
 
-    // i would like to print a statment that says thank you, I'll get back to you shortly
+  // Print the value so you can see it in the server logs.
+   // System.out.println("You submitted: " + timestamp);
+
+    // Write the value to the response so the user can see it.
+   // response.getWriter().println("You submitted: " + email);
+
   }
 }
